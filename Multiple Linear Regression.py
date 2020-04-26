3d33731038b365f443deb029d11b4d2ee961c402
Mitul Prajapati <63492181+mitulprajapati@users.noreply.github.com>
2020-04-26 19:06:30 +1000

    Multiple_linear_Regression

---
+++ Multiple Linear Regression.py
@@ -0,0 +1,26 @@
+import numpy as np
+import pandas as pd
+import matplotlib as plt
+# importing the dataset
+df = pd.read_csv('50_Startups.csv')
+x = df.iloc[:, :-1].values
+y = df.iloc[:, -1].values
+
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import OneHotEncoder
+
+ct = ColumnTransformer(transformers=[('encode', OneHotEncoder(), [3])], remainder='passthrough')
+x = np.array(ct.fit_transform(x))
+
+from sklearn.model_selection import train_test_split
+
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=0)
+
+from sklearn.linear_model import LinearRegression
+
+regressor = LinearRegression()
+regressor.fit(x_train, y_train)
+
+y_pred = regressor.predict(x_test)
+np.set_printoptions(precision=2)
+print(np.concatenate((y_pred.reshape(len(y_pred), 1), y_test.reshape(len(y_test), 1)), 1))
